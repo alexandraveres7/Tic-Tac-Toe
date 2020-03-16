bac3ea0b6e9a9362df7a6331aa98012beeb62502
alexandraveres7 <alexandraveres7@gmail.com>
2020-03-16 18:01:30 +0200

    multiple pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LeaderboardsComponent } from './leaderboards/leaderboards.component';
+import { BoardComponent } from './board/board.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'playboard', component: BoardComponent},
+  {path: 'leaderboard', component: LeaderboardsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
